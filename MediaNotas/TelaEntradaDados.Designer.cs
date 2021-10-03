678e849bd2b2a8a3ea1914af5ba445771df86596
Tamires <tamirespatrocinioo@gmail.com>
2021-10-02 23:16:33 -0300

    Adicionar arquivos de projeto.

---
+++ MediaNotas/TelaEntradaDados.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace MediaNotas
+{
+    partial class TelaEntradaDados
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.dateTimePickerNascimento = new System.Windows.Forms.DateTimePicker();
+            this.numericNota1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.numericNota2 = new System.Windows.Forms.NumericUpDown();
+            this.numericNota3 = new System.Windows.Forms.NumericUpDown();
+            this.numericNota4 = new System.Windows.Forms.NumericUpDown();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.listBoxDados = new System.Windows.Forms.ListBox();
+            this.numericMedia = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericNota1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericNota2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericNota3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericNota4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericMedia)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans Semibold", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(207, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cálculo da Média dos Alunos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(76, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(446, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Data de Nascimento:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(76, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Insira as notas:";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxNome.Location = new System.Drawing.Point(80, 126);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(324, 29);
+            this.textBoxNome.TabIndex = 4;
+            // 
+            // dateTimePickerNascimento
+            // 
+            this.dateTimePickerNascimento.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePickerNascimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerNascimento.Location = new System.Drawing.Point(452, 126);
+            this.dateTimePickerNascimento.Name = "dateTimePickerNascimento";
+            this.dateTimePickerNascimento.Size = new System.Drawing.Size(181, 29);
+            this.dateTimePickerNascimento.TabIndex = 5;
+            // 
+            // numericNota1
+            // 
+            this.numericNota1.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericNota1.Location = new System.Drawing.Point(319, 220);
+            this.numericNota1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericNota1.Name = "numericNota1";
+            this.numericNota1.Size = new System.Drawing.Size(38, 29);
+            this.numericNota1.TabIndex = 6;
+            this.numericNota1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(216, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 22);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Atividade 1:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(216, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 22);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Atividade 2:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(216, 286);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 22);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Atividade 3:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(216, 318);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 22);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Atividade 4:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(234, 385);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 22);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Média:";
+            // 
+            // numericNota2
+            // 
+            this.numericNota2.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericNota2.Location = new System.Drawing.Point(319, 252);
+            this.numericNota2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericNota2.Name = "numericNota2";
+            this.numericNota2.Size = new System.Drawing.Size(38, 29);
+            this.numericNota2.TabIndex = 12;
+            this.numericNota2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numericNota3
+            // 
+            this.numericNota3.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericNota3.Location = new System.Drawing.Point(319, 284);
+            this.numericNota3.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericNota3.Name = "numericNota3";
+            this.numericNota3.Size = new System.Drawing.Size(38, 29);
+            this.numericNota3.TabIndex = 13;
+            this.numericNota3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numericNota4
+            // 
+            this.numericNota4.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericNota4.Location = new System.Drawing.Point(319, 316);
+            this.numericNota4.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericNota4.Name = "numericNota4";
+            this.numericNota4.Size = new System.Drawing.Size(38, 29);
+            this.numericNota4.TabIndex = 14;
+            this.numericNota4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnCalcular.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCalcular.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnCalcular.FlatAppearance.BorderSize = 2;
+            this.btnCalcular.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btnCalcular.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Fuchsia;
+            this.btnCalcular.Font = new System.Drawing.Font("Open Sans Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCalcular.Location = new System.Drawing.Point(452, 383);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(117, 29);
+            this.btnCalcular.TabIndex = 15;
+            this.btnCalcular.Text = "Gerar";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // listBoxDados
+            // 
+            this.listBoxDados.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listBoxDados.FormattingEnabled = true;
+            this.listBoxDados.ItemHeight = 22;
+            this.listBoxDados.Location = new System.Drawing.Point(80, 471);
+            this.listBoxDados.Name = "listBoxDados";
+            this.listBoxDados.Size = new System.Drawing.Size(553, 158);
+            this.listBoxDados.TabIndex = 17;
+            // 
+            // numericMedia
+            // 
+            this.numericMedia.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericMedia.Location = new System.Drawing.Point(319, 383);
+            this.numericMedia.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericMedia.Name = "numericMedia";
+            this.numericMedia.Size = new System.Drawing.Size(38, 29);
+            this.numericMedia.TabIndex = 18;
+            // 
+            // TelaEntradaDados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(713, 673);
+            this.Controls.Add(this.numericMedia);
+            this.Controls.Add(this.listBoxDados);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.numericNota4);
+            this.Controls.Add(this.numericNota3);
+            this.Controls.Add(this.numericNota2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericNota1);
+            this.Controls.Add(this.dateTimePickerNascimento);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "TelaEntradaDados";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tela Média Notas";
+            ((System.ComponentModel.ISupportInitialize)(this.numericNota1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericNota2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericNota3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericNota4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericMedia)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.DateTimePicker dateTimePickerNascimento;
+        private System.Windows.Forms.NumericUpDown numericNota1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericNota2;
+        private System.Windows.Forms.NumericUpDown numericNota3;
+        private System.Windows.Forms.NumericUpDown numericNota4;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ListBox listBoxDados;
+        private System.Windows.Forms.NumericUpDown numericMedia;
+    }
+}
+
---
+++ MediaNotas/TelaEntradaDados.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaNotas
+{
+    public partial class TelaEntradaDados : Form
+    {
+        public TelaEntradaDados()
+        {
+            InitializeComponent();
+        }
+
+        private decimal MediaAluno (params decimal[] notas)
+        {
+            decimal soma = 0;
+            for(int i=0; i <=notas.GetLength(0) - 1; i++)
+            {
+                soma += notas[i];
+            }
+
+            return soma / notas.Length;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            listBoxDados.Items.Clear();
+            string nome = textBoxNome.Text;
+            string nascimento = dateTimePickerNascimento.Value.ToString();
+            decimal nota1 = numericNota1.Value;
+            decimal nota2 = numericNota2.Value;
+            decimal nota3 = numericNota3.Value;
+            decimal nota4 = numericNota4.Value;
+
+
+            listBoxDados.Items.Add("Aluno(a): " + nome);
+
+            listBoxDados.Items.Add("Data de Nascimento: " + nascimento);
+
+            listBoxDados.Items.Add("Nota da atividade 1 = " + nota1);
+            listBoxDados.Items.Add("Nota da atividade 2 = " + nota2);
+            listBoxDados.Items.Add("Nota da atividade 3 = " + nota3);
+            listBoxDados.Items.Add("Nota da atividade 4 = " + nota4);
+            listBoxDados.Items.Add("Média Final: " + MediaAluno(nota1, nota2, nota3, nota4));
+
+            decimal media = MediaAluno(nota1, nota2, nota3, nota4);
+
+            decimal valormedia = numericMedia.Value;
+
+            if(media >= valormedia)
+            {
+                MessageBox.Show("Aluno Aprovado! :D");
+            }
+            else
+            {
+                MessageBox.Show("Aluno Reprovado! :(");
+            }
+        }
+    }
+}
